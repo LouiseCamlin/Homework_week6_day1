5e4281602efadaec910b62d2c0a5088319bdfb93
Louise Camlin <lacamlin@gmail.com>
2017-05-16 17:38:55 +0100

    add and remove functions working

---
+++ day2/PhotographerTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.*;
+
+public class PhotographerTest {
+  Photographer photographer;
+  AnalogCamera analogCamera;
+  DigitalCamera digitalCamera;
+
+  @Before
+  public void before(){
+    photographer = new Photographer();
+    analogCamera = new AnalogCamera();
+    digitalCamera = new DigitalCamera();
+  }
+
+  @Test
+  public void bagStartsEmpty() {
+    assertEquals(0, photographer.cameraCount());
+  }
+
+  @Test
+  public void canAddCamera() {
+    photographer.addCamera(analogCamera);
+    assertEquals(1, photographer.cameraCount());
+  }
+
+  @Test
+  public void canAddMoreThanOneCamera() {
+    photographer.addCamera(analogCamera);
+    photographer.addCamera(digitalCamera);
+    assertEquals(2, photographer.cameraCount());
+  }
+
+  @Test
+  public void canRemoveCamera() {
+    photographer.addCamera(analogCamera);
+    Printable camera = photographer.removeCamera();
+    assertNotNull(camera);
+  }
+
+  @Test
+  public void canRemoveDigitalCamera() {
+    photographer.addCamera(digitalCamera);
+    Printable camera = photographer.removeCamera();
+    DigitalCamera original = (DigitalCamera) camera;
+    assertEquals("nada", original.printDetails());
+  }
+
+}
